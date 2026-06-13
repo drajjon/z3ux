4cdc7f9ce42f480797c78e1dda61d0a31c8c09d7
Alexis Janson <133168946+swajj@users.noreply.github.com>
2026-06-12 18:34:42 -0700

    coroutines

--- protoedge/yarn.lua
+++ protoedge/yarn.lua
@@ -1,8 +1,9 @@
 ---@class YarnProto
----@field tick fun(self:Yarn)? Called once per tick, i.e. at 60 FPS
+---@field tick fun(self:Yarn)? Called or resumed once per tick, i.e. at 60 FPS
 
 ---@class Yarn
 ---@field id integer Unique ID per Yarn [READONLY]
+---@field _coro thread?
 local Yarn = {}
 local meta_Yarn = { __index = Yarn }
 local id_order = 1
@@ -16,11 +17,25 @@ local function new_Yarn(opts)
     return self
 end
 
--- You can override this in your new Yarn to make a simple fixed_update Yarn.
+-- You can override this in your new Yarn to make a simple fixed_update Yarn that does not use coroutines.
 -- You may call it manually to speed ahead if you like.
--- The default implementation
-function Yarn:tick()
+function Yarn:run_tick()
+    local coro = self._coro
+    if coro then
+        local status = coroutine.status(coro)
+        if status == 'dead' then
+            coro = nil -- Need a new coroutine
+        end
+    end
+    if not coro then
+        coro = coroutine.create(self.tick)
+        self._coro = coro
+    end
+    coroutine.resume(coro, self)
+end
 
+-- Safety default implementation, does nothing
+function Yarn:tick()
 end
 
 return new_Yarn
